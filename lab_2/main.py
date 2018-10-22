f90e1d4f00511e4de630c749623d6c312b284663
KsenyaSH <43419770+KsenyaSH@users.noreply.github.com>
2018-10-22 20:16:18 +0300

    all functions

--- lab_2/main.py
+++ lab_2/main.py
@@ -11,6 +11,67 @@ if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
         freq_dict = calculate_frequences(REFERENCE_TEXT)
+
+def propose_candidates(word: str, max_depth_permutations: int=1) -> list:
+    if type(word) == str and word != '' and max_depth_permutations == 1:
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z']
+        lst1 = []
+        lst4 = []
+
+        # удаление 1-ой буквы
+        lst2 = list(word)
+        lst3 = lst2.copy()
+        for i in range(len(lst2)):
+            del (lst3[i])
+            word2 = ''.join(lst3)
+            lst4.append(word2)
+            lst3 = lst2.copy()
+
+        # добавить букву в строку
+        lst5 = [i for i in word]
+        lst6 = lst5.copy()
+        lst7 = []
+        for i in alphabet:
+            for k in range(len(lst5)):
+                lst6.insert(k, i)
+                word3 = ''.join(lst6)
+                lst7.append(word3)
+                lst6 = lst5.copy()
+
+        lst8 = [word + i for i in alphabet]
+        lst9 = lst7 + lst8
+
+
+        # замена 1 буквы
+        word1 = word
+        for i in word1:
+            word1 = word
+            for k in alphabet:
+                a = word1.replace(i, k)
+                lst1.append(a)
+
+        # поменять буквы местами
+
+        lst10 = [i for i in word]
+        lst11 = lst10.copy()
+        lst12 = []
+
+        for i in range(len(lst10) - 1):
+            lst11[i], lst11[i + 1] = lst11[i + 1], lst11[i]
+            word4 = ''.join(lst11)
+            lst12.append(word4)
+            lst11 = lst10.copy()
+
+        totallist = lst4 + lst9 + lst1 + lst12
+        nodubl = set(totallist)
+        candidates = list(nodubl)
+
+
+    else:
+        candidates = []
+    return candidates
+
 def keep_known(candidates: tuple, frequencies: dict) -> list:
     lst = []
     if type(frequencies)== dict:
@@ -46,3 +107,22 @@ def choose_best(frequencies: dict, candidates: tuple) -> str:
         finalresult = lst1[0][0]
 
     return finalresult
+
+def spell_check_word(frequencies: dict, as_is_words: tuple, word: str) -> str:
+    if type(frequencies) != dict:
+        result = 'UNK'
+    elif type(as_is_words) != tuple:
+        candidates = propose_candidates(word, 1)
+        candidates = tuple(candidates)
+        result = choose_best(frequencies, candidates)
+    elif type(word) != str:
+        result = 'UNK'
+    elif (word in as_is_words) or (word in frequencies) or (word.upper() in as_is_words) or (word.lower() in as_is_words):
+        result = word
+    else:
+        candidates = propose_candidates(word, 1)
+        candidates = tuple(candidates)
+        result = choose_best(frequencies, candidates)
+
+    return result
+
